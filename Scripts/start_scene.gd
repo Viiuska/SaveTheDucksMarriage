4718e255cd5e0b9290e783c113c906ab6c3ce6cb
Viiuska <87257685+Viiuska@users.noreply.github.com>
2025-11-04 18:16:31 +0200

    Update start_scene
    After dying in other levels, level 1 shows correct collectable

--- Scripts/start_scene.gd
+++ Scripts/start_scene.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal OnUpdateView(score : int, typeAmount : int, type: String)
+
 @onready var anim : AnimationPlayer = $AnimationPlayer
 
 var animation_played : bool = false
@@ -15,7 +17,15 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	call_deferred("_load_scene")
 
 func _on_skip_button_pressed():
-	call_deferred("_load_scene")
+	_emit_update_signals()
+	_load_scene()
+
+
+func _emit_update_signals():
+	PlayerCollects.score = 0
+	PlayerCollects.typeAmount = 3
+	PlayerCollects.type = "bFlower"
+	OnUpdateView.emit(PlayerCollects.score, PlayerCollects.typeAmount, PlayerCollects.type)
 
 func _load_scene():
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
